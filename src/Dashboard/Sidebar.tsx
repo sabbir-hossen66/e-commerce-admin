b6b1650d73ce9c24bc7aac129f91a86cec0f13c1
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-09-12 22:02:39 +0600

    sidebar creating

---
+++ src/Dashboard/Sidebar.tsx
@@ -0,0 +1,8 @@
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h2>Sidebar</h2>
+    </div>
+  );
+}
+export default Sidebar;
--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,7 +1,10 @@
+import Sidebar from "../Dashboard/Sidebar"
+
 export const Home=()=>{
   return(
     <div>
       <h2>hello home</h2>
+      <Sidebar/>
     </div>
   )
 }
